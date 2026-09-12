2ae41d7f5ef8a604ba9416a36d21c0a9d703e5c1
Matt <hessmodee@gmail.com>
2026-09-11 22:51:38 -0600

    HQ P0 painter: refuse center OUTCOME_PROVEN (stripe-only)
    * HQ P0: cache-bust painter v=4 + sw v14 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: add .github/workflows/hq-offline-resilience-v1-proof.yml (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 01/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 02/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 03/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 04/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 05/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 06/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 07/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 08/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 09/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 10/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 11/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 12/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 13/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 14/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 15/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: painter b64 part 16/16 (refuse OUTCOME_PROVEN center)
    
    * HQ P0: add docs/ops/test-hq-p0-floor-painter-v1.cjs (refuse OUTCOME_PROVEN center)

--- docs/sw.js
+++ docs/sw.js
@@ -8,7 +8,7 @@
  */
 'use strict';
 
-const VERSION='kevin-hq-shell-v13';
+const VERSION='kevin-hq-shell-v14';
 const SHELL_CACHE=`${VERSION}-static`;
 const DATA_CACHE=`${VERSION}-public-data`;
 const BASE='/KEVIN-WORK/';
